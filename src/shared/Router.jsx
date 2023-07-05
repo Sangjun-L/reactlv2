23ca2e52b01024b88df80dc17c22dbf21ba2ebba
DESKTOP-281HJUM\pilot <pilot03067@naver.com>
2023-07-05 18:31:35 +0900

    리액트 레벨 2 완성

---
+++ src/shared/Router.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import Detail from "../pages/Detail";
+import Home from "../pages/Home";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
